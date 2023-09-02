b99d127f29897ccb86bc5533b550934d062f9dc6
Ramesh88899 <129539016+Ramesh88899@users.noreply.github.com>
2023-09-02 11:19:40 +0530

    Add files via upload

---
+++ lecture 5 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a=12, b=3;
+	int addition, subtraction, multiplication , division, modulus;
+	addition=a+b;
+	subtraction=a-b;
+	multiplication=a*b;
+	division=a/b;
+	modulus=a%b;
+	printf("%d", addition);
+	printf("\n%d", subtraction);
+	printf("\n%d", multiplication);
+	printf("\n%d", division);
+	printf("\n%d", modulus);
+	
+	
+}
