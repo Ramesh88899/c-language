5f6024e1e666a516c5d4f899c9a38c2543b65465
Ramesh88899 <129539016+Ramesh88899@users.noreply.github.com>
2023-10-26 14:56:16 +0530

    Add files via upload

---
+++ malloc function.c
@@ -0,0 +1,31 @@
+//malloc()
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+	int *p, n, i;
+	
+printf("enter the number of integers to be used");
+scanf("%d",&n);
+p=(int*)malloc(n*(sizeof(int)));
+ if(p=NULL)
+ {
+ 	printf("no memory available");
+ 	exit(1); 	
+ }	
+	 else
+	 {
+	 	printf("memory allocationn was successful \n");
+	 	 printf("enter the values \n");
+	 	 for(i=0;i<n;i++)
+    {
+	scanf("%d", p+i );
+	
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d \n",*(p+i)); 
+				}	    	
+     }
+     
+}
